2a42ea187f0b025a3d7d43675f41fdb4ef9f9506
tushardutt <tushardutt.srivastava@gmail.com>
2025-01-10 14:56:39 +0530

    Added Comment

--- src/main/java/com/userManagement/user_registration/service/UserService.java
+++ src/main/java/com/userManagement/user_registration/service/UserService.java
@@ -25,7 +25,7 @@ public class UserService {
         return userRepository.findById(id);
     }
 
-    //Retrieve user by email
+    //Retrieve user by email from Redis
     public User getUserByEmail(String email) {
         return userRepository.findByEmail(email);
     }
